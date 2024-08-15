9b42556ec48b5873099fd191a219f8649a098ec9
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-08-15 12:35:01 -0400

    Add basic pricing page

---
+++ src/app/(root)/pricing/page.tsx
@@ -0,0 +1,61 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+export default function Pricing() {
+  return (
+    <section className="">
+      <div className="container mx-auto bg-slate-300 p-24">
+        <div className="flex flex-col justify-center items-center gap-8">
+          <h1 className="text-3xl">Pricing</h1>
+          <div className="flex justify-center items-center gap-8">
+            <Card className="w-[350px]">
+              <CardHeader>
+                <CardTitle>Basic</CardTitle>
+                <CardTitle className="text-3xl">Free</CardTitle>
+                <CardDescription>
+                  Enjoy limited features of our product!
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul>
+                  <li>Feature 1</li>
+                  <li>Feature 2</li>
+                  <li>Feature 3</li>
+                </ul>
+              </CardContent>
+              <CardFooter>
+                <Button disabled>Current</Button>
+              </CardFooter>
+            </Card>
+            <Card className="w-[350px]">
+              <CardHeader>
+                <CardTitle>Premium</CardTitle>
+                <CardTitle className="text-3xl">$9.99</CardTitle>
+                <CardDescription>
+                  Enjoy all of our product features!
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul>
+                  <li>Feature 1</li>
+                  <li>Feature 2</li>
+                  <li>Feature 3</li>
+                </ul>
+              </CardContent>
+              <CardFooter>
+                <Button>Purchase</Button>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
