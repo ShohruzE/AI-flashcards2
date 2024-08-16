5bb8abf5b885403c8f53d834a9e0aa2e02d570f6
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-08-16 14:48:23 -0400

    Add learn page and protect route to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/create(.*)", "/library(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/create(.*)",
+  "/library(.*)",
+  "/learn(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (!auth().userId && isProtectedRoute(req)) {
