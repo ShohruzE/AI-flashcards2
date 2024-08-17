b4d47ad0906faaeecbb95fc3ef7f06b58ed73e9f
Shohruz Ernazarov <shohruzernazarov04@gmail.com>
2024-08-16 23:57:01 -0400

    Format hero section with image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 import CreateFlashcardsForm from "@/components/CreateFlashcardsForm";
-import honeycomb from "@/images/honeycomb.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <section className="bg-[#5D4037] text-[#FFC107] flex min-h-screen flex-col items-center">
-      <h1 className="font-extrabold text-4xl pt-10 pb-1">Welcome to HoneyComb</h1>
+      <h1 className="font-extrabold text-4xl pt-10 pb-1">
+        Welcome to HoneyComb
+      </h1>
       <p className="text-lg pb-10">Create flashcards instantly using AI!</p>
 
-      <div className="bg-[#FFF3E0] h-max w-9/12 rounded-md p-4 mt-8">
-        <div className="justify-start w-3/6 rounded-md p-4 pl-10">
-          <CreateFlashcardsForm />
+      <div className="bg-[#FFF3E0] h-max w-9/12 rounded-md p-6 mt-8">
+        <div className="flex justify-between items-center rounded-md">
+          <div className="w-[50%]">
+            <CreateFlashcardsForm />
+          </div>
+          <Image
+            width={500}
+            height={400}
+            src="/images/honeycomb.png"
+            alt="HoneyComb header logo"
+          />
         </div>
       </div>
     </section>
